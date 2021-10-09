5093c32a8abf20027aace62c5980f795a2184e2b
limhizy15 <limhizy15@ajou.ac.kr>
2021-10-09 18:02:22 +0900

    [#20] refactor: 선호장르 시각화 데이터 상태관리

--- client/src/components/result/preferenceChart.jsx
+++ client/src/components/result/preferenceChart.jsx
@@ -1,47 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
 
 export default function PreferenceChart({ categories }) {
-  // store에서 할 애들임 지금 임시임
-  const labels = [];
-  const percent = [];
+  const [data, setData] = useState({ labels: [], datasets: [] });
 
-  Object.keys(categories).map((category) => labels.push(category));
-  Object.keys(categories).map((category) => percent.push(categories[category]));
+  useEffect(() => {
+    const labels = [];
+    const percent = [];
 
-  const data = {
-    labels,
-    datasets: [
-      {
-        borderWidth: 1,
-        data: percent,
-      },
-    ],
-  };
+    Object.keys(categories).map((category) => labels.push(category));
+    Object.keys(categories).map((category) =>
+      percent.push(categories[category]),
+    );
 
-  const options = {
-    legend: {
-      display: true, // label 보이기 여부
-    },
-    scales: {
-      yAxes: [
+    const newData = {
+      labels,
+      datasets: [
         {
-          ticks: {
-            min: 0, // y축 스케일에 대한 최소값 설정
-            stepSize: 1, // y축 그리드 한 칸당 수치
-          },
+          borderWidth: 1,
+          data: percent,
         },
       ],
-    },
+    };
 
-    maintainAspectRatio: false,
-  };
+    setData(newData);
+  }, [categories]);
 
   return (
     <div>
       <h3>선호 장르, 카테고리 표</h3>
-      <div>
-        <Bar data={data} options={options} height={300} />
+      <div style={{ width: '600px' }}>
+        <Bar data={data} options={{ maintainAspectRatio: false }} />
       </div>
       {Object.keys(categories).map((category) => (
         <div>
