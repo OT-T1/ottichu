1283df10b7809b861cdadc236438334767249e92
JeongGod <jeonggu223@gmail.com>
2021-10-14 15:47:57 +0900

    [#44] 결과 추천 api 기능 구현

--- server/flaskr/service/result_service.py
+++ server/flaskr/service/result_service.py
@@ -9,76 +9,34 @@ from flaskr.service.recommend_service import Recommendation
 class Result:
     def get_result(user_code):
         recom = Recommendation()
-        data_recom, _ = recom.get_user_data(user_code)
-        data_result = recom.get_recom_result(data_recom)
+        result_data, _ = recom.get_user_data(user_code)
 
-        platform = recom.get_ott_recommendation(data_result)
-        cnt_ott = recom.count_by_ott(data_result)
+        top_platform = recom.get_ott_recommendation(result_data["contents"])
+        top_genre = recom.get_top_genre(result_data["contents"])
 
-        contents_info = (
-            db.session.query(contents)
-            .filter(contents.content_code.in_(data_result))
-            .all()
-        )
-        content_genres = (
-            db.session.query(content_genre)
-            .filter(content_genre.content_code.in_(data_result))
-            .all()
-        )
-        result_genre = defaultdict(int)
-        for info in content_genres:
-            result_genre[info.genre] += 1
-        total_genre_cnt = sum(result_genre.values())
-        result_genre = sorted(result_genre.items(), key=lambda x: x[1], reverse=True)
-        genre = {
-            result_genre[0][0]: result_genre[0][1] / total_genre_cnt,
-            result_genre[1][0]: result_genre[1][1] / total_genre_cnt,
-            result_genre[2][0]: result_genre[2][1] / total_genre_cnt,
-            result_genre[3][0]: result_genre[3][1] / total_genre_cnt,
-            result_genre[4][0]: result_genre[4][1] / total_genre_cnt,
-        }
-        print(genre)
+        print(type(top_platform))
+        print(top_platform)
 
-        platform = {}
+        total_genre_sum = sum(top_genre.values())
+        for key, val in top_genre.items():
+            top_genre[key] = int((val / total_genre_sum) * 100)
 
-        print(data_result)
-        print(len(data_result))
-        print(type(cnt_ott))
-        print(cnt_ott)
-        print(type(platform))
-        print(platform)
-
-        category = {"좀비": 50, "액션": 30, "코미디": 10, "코믹": 5, "호러": 5}
-        platform = {
-            "netflix": {
-                "percentage": 50,
+        total_platform_sum = sum(top_platform.values())
+        for key, val in top_platform.items():
+            top_platform[key] = {
+                "percentage": int((val / total_platform_sum) * 100),
                 "plan": "basic",
                 "quality": "4K",
                 "price": 5000,
                 "people_number": 4,
-            },
-            "watcha": {
-                "percentage": 50,
-                "plan": "basic",
-                "quality": "4K",
-                "price": 5000,
-                "people_number": 4,
-            },
-            "coupang": {
-                "percentage": 50,
-                "plan": "basic",
-                "quality": "4K",
-                "price": 5000,
-                "people_number": 4,
-            },
-        }
+            }
 
-        return jsonify(category=category, platform=platform), 200
+        return jsonify(category=top_genre, platform=top_platform), 200
 
     def get_wordcloud(user_code):
         recom = Recommendation()
-        data_recom, _ = recom.get_user_data(user_code)
-        result = recom.get_recom_result(data_recom)
+        result_data, _ = recom.get_user_data(user_code)
+        result = recom.get_ott_recommendation(result_data["contents"])
         tokens = recom.for_wordcloud(result)
 
         token_cnt = Counter(tokens.split())
